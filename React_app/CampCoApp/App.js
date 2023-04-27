0f9c29fd20ea66704d17bdaa19e741f4a66524c8
juliettevoskian <julvoskian@gmail.com>
2023-04-26 23:27:38 -0400

    added files

--- React_app/CampCoApp/App.js
+++ React_app/CampCoApp/App.js
@@ -5,10 +5,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar bnkcvstyle="auto" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
--- React_app/CampCoApp/Munchies.js
+++ React_app/CampCoApp/Munchies.js
@@ -1,5 +1,10 @@
 import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity, Pressable } from 'react-native';
 import { useEffect, useState } from 'react';
+import { Button, ListItem } from 'react-native-elements'; 
+
+const MenuScreen = () => {
+    const [menuItems, setMenuItems] = useState([]);
+}
 
 export default function Munchies({ navigation, route }) {
     menu = [
@@ -12,3 +17,51 @@ export default function Munchies({ navigation, route }) {
         {menuitem: "NACHOS", menuprice: "$8.49"},
     ]
 }
+const handleAddToOrder = item => {
+  };
+  
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Menu</Text>
+      <FlatList
+        data={menuItems}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <ListItem>
+            <ListItem.Content>
+              <ListItem.Title>{item.name}</ListItem.Title>
+              <ListItem.Subtitle>${item.price.toFixed(2)}</ListItem.Subtitle>
+            </ListItem.Content>
+            <Button
+              title="Add to Order"
+              onPress={() => handleAddToOrder(item)}
+              buttonStyle={styles.button}
+            />
+          </ListItem>
+        )}
+      />
+    </View>
+  );
+
+
+            
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#fff',
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    button: {
+      backgroundColor: '#007aff',
+      borderRadius: 10,
+      paddingHorizontal: 20,
+    },
+  });
+  
+
