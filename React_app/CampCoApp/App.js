a39ca4033e9c0312b3a512f2fdfeef08a874f02e
Joseph Fantasia <jfantasi@villanova.edu>
2023-04-27 17:44:46 -0400

    added files

--- React_app/CampCoApp/App.js
+++ React_app/CampCoApp/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react'
+import { StyleSheet, Text, View, Image, Button, Pressable, NavigationContainer } from 'react-native';
+import Munchies from './Munchies';
+import HomePage from './HomeScreen';
 
-export default function App() {
+export default function App({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomePage/>
     </View>
   );
 }
@@ -17,4 +19,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginBottom: 600,
+    marginTop: 125,
+  },
+  button: {
+    fontSize: 300,
+    marginBottom: 400,
+
+  },
+
 });
+
---
+++ React_app/CampCoApp/HomeScreen.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import App from './TrialPizza'
+
+export default function HomePage({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: 'https://apis.google.com/_/scs/abc-static/_/js/k=gapi.gapi.en.UjJbvPIecP0.O/m=gapi_iframes,googleapis_client/rt=j/sv=1/d=1/ed=1/rs=AHpOoo_flbzE3yQmWQ7n7N3yCQZtJt8-oA/cb=gapi.loaded_0' }}
+        style={styles.logo}
+      />
+      {/* <Text style={styles.title}>Welcome to Campus Corner</Text> */}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('App', { type: 'delivery' })}
+      >
+        <Text style={styles.buttonText}>Order Delivery</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Munchies', { type: 'pickup' })}
+      >
+        <Text style={styles.buttonText}>Order Pickup</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: '#0000FF',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 10,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
