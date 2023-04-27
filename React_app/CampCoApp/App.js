b8b3cebedde5f4f270a00f14120d6f7572cc0c4b
Joseph Fantasia <jfantasi@villanova.edu>
2023-04-27 17:54:59 -0400

    App Changes

--- React_app/CampCoApp/App.js
+++ React_app/CampCoApp/App.js
@@ -1,27 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react'
-import { StyleSheet, Text, View, Image, Button, Pressable, NavigationContainer } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, NavigationContainer } from 'react-native';
 import Munchies from './Munchies';
 import HomePage from './HomeScreen';
 
-<<<<<<< HEAD
-export default function App({navigation}) {
+export default function App({}) {
   return (
     <View style={styles.container}>
       <HomePage/>
     </View>
   );
-=======
-
-
-export default function App() {
- return (
-   <View style={styles.container}>
-    <Text>Open up App.js to start working on your app!</Text>
-     <StatusBar bnkcvstyle="auto" />
-   </View>
-);
->>>>>>> a5d72d66e10b769df7258da2dfcf1b83292aa6f5
 }
 
 
